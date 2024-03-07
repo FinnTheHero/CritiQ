3ee496a0031a2e9db464279ef659057d9b588460
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-03-07 18:20:30 +0400

    Added custom styles

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
 // Bootstrap components
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
 
 const DisplayCard = () => {
 	return (
-		<Card bg="dark" text="white" className='Card'>
+		<Card text="white" className='Card'>
 			<Card.Img src="logo512.png" className='Image'/>
-			<Card.Body style={{textAlign: 'left'}}>
-				<Card.Title>Name</Card.Title>
-				<Card.Text>
+			<Card.Body className='CardBody'>
+				<Card.Title className='Title'>Name</Card.Title>
+				<Card.Text className='Text'>
 				Some information.
 				</Card.Text>
+				<div className='Extra'>
+					<h5 className='Subtitle my-auto'>10 Stars</h5>
+					<button className='Button'>More</button>
+				</div>
 			</Card.Body>
 		</Card>
 	);
 };
 
 export default DisplayCard;
-
-
-
-    
